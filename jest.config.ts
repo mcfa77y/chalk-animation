111836a7246970f04e150a09718e2c6314744677
Joe Lau <mcfa77y@gmail.com>
2025-03-09 21:04:46 -0700

    update packages; use pnpm

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   slowTestThreshold: 10,
   roots: ["<rootDir>"],
   testMatch: ["**/__tests__/*.test.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+  testEnvironment: "node",
 };
 
 export default config;
